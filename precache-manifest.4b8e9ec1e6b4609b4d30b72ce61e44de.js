60f90999f5385820d80398537debd01c058e21dd
Bishal <bishal@192-168-1-111.tpgi.com.au>
2020-08-17 19:16:42 +1000

    Updates

---
+++ precache-manifest.4b8e9ec1e6b4609b4d30b72ce61e44de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f30e582750f5be22227c482edfb8329",
+    "url": "/toosmate-web/index.html"
+  },
+  {
+    "revision": "fb436b24bd8e43dfc543",
+    "url": "/toosmate-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19e0c43bd7db5322cbba",
+    "url": "/toosmate-web/static/js/2.bd24de33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toosmate-web/static/js/2.bd24de33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb436b24bd8e43dfc543",
+    "url": "/toosmate-web/static/js/main.95f57981.chunk.js"
+  },
+  {
+    "revision": "e751e8d787a8cd82da7e",
+    "url": "/toosmate-web/static/js/runtime-main.20436f7a.js"
+  }
+]);
