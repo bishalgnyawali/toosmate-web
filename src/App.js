bef16d271ab929e0bf111f4b70c3605b77b2ac94
Bishal <bishal@192-168-1-111.tpgi.com.au>
2020-08-17 18:59:47 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Nav from './components/navBar/Nav'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav name={"Tradies"} slogan={"Come to our Business"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navBar/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = (props) => {
+    return (
+        <>
+            <h1>{props.name}</h1>
+            <p>{props.slogan}</p>
+        </>
+    )
+}
+export default Nav;
